4f5f6fd998e95553e14c6e7078449e84ef2244fa
r-sie <32829606+Arsiela@users.noreply.github.com>
2026-08-07 17:01:23 +0800

    08072026

--- src/main/java/org/guanzon/cas/purchasing/controller/PurchaseOrder.java
+++ src/main/java/org/guanzon/cas/purchasing/controller/PurchaseOrder.java
@@ -389,15 +389,6 @@ public class PurchaseOrder extends Transaction {
             Master().setTransactionNo(Master().getNextCode());
 
         }
-        
-        //Require Attachment if transaction total is greater than 100,000.0000 Arsiela 08-07-2026 04:44PM Requested by sir Mac
-        if(Master().getTranTotal().doubleValue() > 100000.0000){
-            if(getTransactionAttachmentCount() <= 0){
-                poJSON.put("result", "error");
-                poJSON.put("message", "Transaction attachment cannot be empty.");
-                return poJSON;
-            }
-        }
 
         if (poGRider.getDepartment().equals(allowedDepartment)) {
             if (Master().getTransactionStatus().equals(PurchaseOrderStatus.OPEN)
@@ -825,6 +816,15 @@ public class PurchaseOrder extends Transaction {
                 poJSON = isEntryOkay(lsStatus);
                 if (!"success".equals((String) poJSON.get("result"))) {
                     return poJSON;
+                }
+                
+                //Require Attachment if transaction total is greater than 100,000.0000 Arsiela 08-07-2026 04:44PM Requested by sir Mac
+                if(Master().getTranTotal().doubleValue() > 100000.0000){
+                    if(getTransactionAttachmentCount() <= 0){
+                        poJSON.put("result", "error");
+                        poJSON.put("message", "Transaction attachment cannot be empty.");
+                        return poJSON;
+                    }
                 }
 
                 //get the matrix return from isEntryOkey
