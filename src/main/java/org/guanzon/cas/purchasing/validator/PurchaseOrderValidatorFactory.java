f8685ab446a3e8b61067a4b7525fc0a8c6cce497
Jeitee <93184955+jeitee@users.noreply.github.com>
2026-08-15 13:49:04 +0800

    08152026

--- src/main/java/org/guanzon/cas/purchasing/validator/PurchaseOrderValidatorFactory.java
+++ src/main/java/org/guanzon/cas/purchasing/validator/PurchaseOrderValidatorFactory.java
@@ -1,21 +1,32 @@
 package org.guanzon.cas.purchasing.validator;
 
 import org.guanzon.appdriver.iface.GValidator;
+import org.guanzon.cas.purchasing.status.PurchaseOrderStatus;
 
 public class PurchaseOrderValidatorFactory {
 
     public static GValidator make(String industryId) {
         switch (industryId) {
-            case "01": //Mobile Phone
-                return new PurchaseOrder_MP();
-            case "02": //Motorcycle
+            case PurchaseOrderStatus.IndustryCode.MOTORCYCLE:
                 return new PurchaseOrder_MC();
-            case "03": //Vehicle
+
+            case PurchaseOrderStatus.IndustryCode.MOBILEPHONE:
+                return new PurchaseOrder_MP();
+
+            case PurchaseOrderStatus.IndustryCode.AUTO_GROUP_HONDA:
+            case PurchaseOrderStatus.IndustryCode.AUTO_GROUP_ANY:
+            case PurchaseOrderStatus.IndustryCode.AUTO_GROUP_NISSAN:
                 return new PurchaseOrder_Vehicle();
-            case "04": //Hospitality
+
+            case PurchaseOrderStatus.IndustryCode.MONARCH:
                 return new PurchaseOrder_Hospitality();
-            case "05": //Los Pedritos
+
+            case PurchaseOrderStatus.IndustryCode.PEDRITOS:
                 return new PurchaseOrder_LP();
+
+            case PurchaseOrderStatus.IndustryCode.GENERAL:
+                return new PurchaseOrder_LP();
+
             default:
                 return new PurchaseOrder_General();
         }
